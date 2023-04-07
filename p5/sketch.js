b49fb3f39cb85792cdde4f3fe0182226f898b9aa
R4chel <rae84@cornell.edu>
2023-04-07 16:05:13 -0400

    update shapes

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,34 +1,51 @@
-let circles = [];
+let shapes;
 let colors;
 
 const maxRadiusRatio = 5;
 const minRadiusRatio = 500;
 let minRadius, maxRadius;
+const initialShapes = 100;
+
+function MyShape({
+    x,
+    y,
+    r
+}) {
+    this.p = createVector(x, y)
+    this.acceleration = createVector(0, 0)
+    this.velocity = p5.Vector.random2D();
+    this.color = random(colors)
+    this.r = r
+
+    this.draw = function() {
+        fill(this.color);
+        circle(this.p.x, this.p.y, this.r);
+    };
+
+    this.update = function() {
+        this.acceleration.add(random(-0.01, 0.01), random(-0.01, 0.01))
+        this.velocity.add(this.acceleration)
+        this.p.add(this.velocity)
+    }
+
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
     minRadius = min(width, height) / minRadiusRatio;
     maxRadius = max(width, height) / maxRadiusRatio;
+    ellipseMode(RADIUS);
 
     colorMode(RGB, 255);
     colors = [color(236, 232, 125), color(52, 115, 76), color(83, 176, 193)];
 
     noStroke();
-}
-
-function spawnCircle() {
-    let x = random(width);
-    let y = random(height);
-    let r = maxSize(x, y) * random(0.25, 0.95);
-    if (r > minRadius) {
-        circles.push({
-            x: x,
-            y: y,
-            r: r,
-            color: random(colors)
-        });
-    }
+    shapes = [...new Array(initialShapes)].map(() => new MyShape({
+        x: random(width),
+        y: random(height),
+        r: 20
+    }));
 }
 
 function maxSize(x, y) {
@@ -38,17 +55,17 @@ function maxSize(x, y) {
         let distance = dist(x, y, c.x, c.y);
         m = min([distance - c.r / 2, m]);
     }
-    return m * 2;
+    return m;
 }
 
 function draw() {
     background(240);
 
-    for (let i = 0; i < circles.length; i++) {
-        let c = circles[i];
-        fill(c.color);
-        circle(c.x, c.y, c.r);
+    for (let i = 0; i < shapes.length; i++) {
+        let s = shapes[i];
+        s.draw();
+        s.update();
     }
 
-    spawnCircle();
+
 }
